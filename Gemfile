e1d1eb85aa98c336108cf68b2c24f931a8087167
thertus <thertus@opswat.com>
2014-12-05 12:14:08 -0800

    Initial commit - App which is calling OpenWeatherMap API and get current weather information based on caller's IP Address

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org'
+
+gem 'rubygems-update'
+gem 'bundler'
+gem 'sinatra'
+gem 'json'
+gem 'rest-client'
+gem 'shotgun'
---
+++ app.rb
@@ -0,0 +1,53 @@
+require 'rubygems'
+require 'bundler'
+require 'sinatra'
+require 'json'
+require 'rest-client'
+
+set :show_exceptions, false
+
+before do
+  headers 'Access-Control-Allow-Origin' => '*',
+  'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST']
+end
+
+# CONSTANTS
+IP_ADDRESS_API = "http://ip-api.com/json"
+WEATHER_BY_CITY_API = "http://api.openweathermap.org/data/2.5/weather?q="
+
+get '/' do
+  erb :layout
+end
+
+get '/weather' do
+  content_type :json
+  get_weather(get_city_from_ip)
+end
+
+error do
+  "sinatra error handler" 
+end
+
+def get_city_from_ip
+ begin
+  ip_address_info = JSON.parse RestClient.get(IP_ADDRESS_API)
+  sanitize_city_name ip_address_info["city"]
+ rescue => e
+
+ end
+end
+
+def get_weather(city_name)
+  begin
+    return RestClient.get WEATHER_BY_CITY_API + city_name
+  rescue => e
+
+  end
+end
+
+def sanitize_city_name city_name
+  clean_name = city_name.downcase
+  clean_name.gsub! ' ', '+'  
+end
+
+
